3384108c369bfa3ae785c368167538867e691c9c
izanamiz <vanhtran112@gmail.com>
2023-04-05 16:22:49 +0700

    3rd

--- js/main.js
+++ js/main.js
@@ -1,17 +1,49 @@
 function backToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
-// const modal = document.getElementsByClassName("modal");
-// const overlay = document.getElementsByClassName("overlay");
-// console.log(modal);
 
 function displayBlock() {
   document.querySelector(".modal").style.display = "block";
-  document.querySelector(".overlay").style.display = "block";
-  document.body.style.overflow="hidden";
+
+  const btn = document.querySelector(".modal-btn-icon");
+  btn.removeChild(btn.firstElementChild);
+
+  let iNode = document.createElement("i");
+  iNode.className = "fa-solid fa-xmark fa-2xl close";
+  iNode.setAttribute("onclick", "displayNone()");
+  iNode.style.fontSize = "3em";
+  btn.appendChild(iNode);
 }
 function displayNone() {
   document.querySelector(".modal").style.display = "none";
-  document.querySelector(".overlay").style.display = "none";
-  document.body.style.overflow="auto";
+
+  const btn = document.querySelector(".modal-btn-icon");
+  btn.removeChild(btn.firstElementChild);
+
+  let iNode = document.createElement("i");
+  iNode.className = "fa-solid fa-bars fa-2xl close";
+  iNode.setAttribute("onclick", "displayBlock()");
+  iNode.style.fontSize = "3em";
+  btn.appendChild(iNode);
+}
+
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  const btt = document.querySelector(".to-top");
+  console.log(document.body.scrollTop);
+  if (document.body.scrollTop > 41 || document.documentElement.scrollTop > 41) {
+    document.querySelector(".navbar").style.backgroundColor = "#e4e7ff";
+  } else {
+    document.querySelector(".navbar").style.backgroundColor = "transparent";
+  }
+  if (
+    document.body.scrollTop > 700 ||
+    document.documentElement.scrollTop > 700
+  ) {
+    btt.style.display = "block";
+  } else {
+    btt.style.display = "none";
+  }
 }
